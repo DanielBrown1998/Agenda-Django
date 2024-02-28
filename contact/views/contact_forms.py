deb83b6e2044b4b165a71bd32944f487fdc9579d
Daniel Brown Rodrigues Mingozzi dos Passos <119956376+DanielBrown1998@users.noreply.github.com>
2024-02-28 17:59:16 -0300

    update forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -85,7 +85,16 @@ def delete(request, contact_id):
         pk=contact_id,
         show=True
     )
+    confirmation = request.POST.get('confirmation', 'no')
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect("contact:index")
+
     return render(
         request,
-        "contact/index.html"
+        "contact/contact.html",
+        {
+            "contact": contact,
+            "confirmation": confirmation,
+        }
     )
